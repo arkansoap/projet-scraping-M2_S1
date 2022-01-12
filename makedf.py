6d54030bbf0e0e7c7894a980361326238bb8f5f6
arkansoap <arkansoap@gmail.com>
2022-01-12 22:21:43 +0100

    makedf.py

--- makedf.py
+++ makedf.py
@@ -6,6 +6,7 @@ from serde import se, serialize, deserialize
 from serde.json import to_json, from_json
 import pandas as pd
 from scrap_main import Sejour, PageElem, CellElem
+from dataclasses import dataclass
 
 
 liste_backup = ["backup_decathlon.json", "backup_explora.json", "backup_terdav.json", "backup_terdav2.json"]
@@ -26,60 +27,67 @@ def assemblage_liste_sejour():
             liste_sejours.append(sejour)
     return liste_sejours
 
-def build_df():
+@dataclass
+class Df:
     """construction du dataframe"""
     columns = ['description', 'prix', 'lieux', 'duree', 'diff', 'theme']
     df = pd.DataFrame(data=assemblage_liste_sejour(), columns= columns)
-    return df 
 
-def make_lower(df):
-    df["theme"] = df["theme"].str.lower()
-    df["description"] = df["description"].str.lower()
-    df["lieux"] = df["lieux"].str.lower()
+    # def build_df():
+    #     """construction du dataframe"""
+    #     columns = ['description', 'prix', 'lieux', 'duree', 'diff', 'theme']
+    #     df = pd.DataFrame(data=assemblage_liste_sejour(), columns= columns)
+    #     return df 
 
-def theme_with_desc(df):
-    for i in range(0, len(df)):
-        if "ski" in df.iloc[i]["description"]:
-            df.loc[i,"theme"] = "activités nordique"
-    # possibilité d'en rajouter
+    def __call__(self):
+        return self
 
-def level_diff(df):
-    df.loc[(df["diff"] ==1)|(df["diff"] ==2)|(df["diff"] =="niveau 1"), "diff"] = "Découverte"
-    df.loc[(df["diff"] ==3)|(df["diff"] =="niveau 2"), "diff"] = "Initié"
-    df.loc[(df["diff"] ==4)|(df["diff"] ==5)|(df["diff"] == "niveau 3")|(df["diff"] == "niveau 4"), "diff"] = "Confirmé"
+    def make_lower(self):
+        self.df["theme"] = self.df["theme"].str.lower()
+        self.df["description"] = self.df["description"].str.lower()
+        self.df["lieux"] = self.df["lieux"].str.lower()
 
-def level_theme(df):
-    df.loc[(df["theme"] =="randonnée")|(df["theme"] =="trek")|(df["theme"] =="auberge")|(df["theme"] =="hôtel")|(df["theme"] =="refuge")|(df["theme"] =="découverte"), "theme"] = "rando nature"
-    df.loc[(df["theme"] == "vélo")|(df["theme"] == "vtt")|(df["theme"] == "vélo de route"), "theme"] = "cyclo"
-    df.loc[(df["theme"] == "randonnée avec âne")|(df["theme"] == "chiens de traîneau")|(df["theme"] == "cani rando"), "theme"] = "ani-rando"
-    df.loc[(df["theme"] =="raquettes à neige")|(df["theme"] =="ski de randonnée")|(df["theme"] =="ski nordique")|(df["theme"] =="chalet")|(df["theme"] =="alpinisme")|(df["theme"] =="cascade de glace")|(df["theme"] =="raquette")|(df["theme"] =="logement privé"), "theme"] = "activités nordique"
-    for i in range(0, len(df)):
-        if "rando nature" not in df.iloc[i]["theme"] and "cyclo" not in df.iloc[i]["theme"] and "activités nordique" not in df.iloc[i]["theme"] and "multi-activités" not in df.iloc[i]["theme"] and "ani-rando" not in df.iloc[i]["theme"]: 
-            df.loc[i,"theme"] = "multi-activités"
+    def theme_with_desc(self):
+        for i in range(0, len(self.df)):
+            if "ski" in self.df.iloc[i]["description"]:
+                self.df.loc[i,"theme"] = "activités nordique"
+        # possibilité d'en rajouter
 
-def level_lieu(df):
-    for i in range(0, len(df)):
-        for str in region:
-            if str in df.loc[i,"lieux"]:
-                df.loc[i, "lieux"] = "metropole"
-        for str in pays_proche:
-            if str in df.loc[i, "lieux"]:
-                df.loc[i, "lieux"] = "dest_proche"
-    for i in range(0, len(df)):
-        if df.loc[i, "lieux"] != "metropole" and df.loc[i, "lieux"] != "dest_proche" and df.loc[i, "lieux"] != "" :
-            df.loc[i, "lieux"] = "dest_éloignée"
+    def level_diff(self):
+        self.df.loc[(self.df["diff"] ==1)|(self.df["diff"] ==2)|(self.df["diff"] =="niveau 1"), "diff"] = "Découverte"
+        self.df.loc[(self.df["diff"] ==3)|(self.df["diff"] =="niveau 2"), "diff"] = "Initié"
+        self.df.loc[(self.df["diff"] ==4)|(self.df["diff"] ==5)|(self.df["diff"] == "niveau 3")|(self.df["diff"] == "niveau 4"), "diff"] = "Confirmé"
 
-def nettoyage(df):
-    df.drop(df[df["prix"]==0].index, axis = 0, inplace=True)
-    df.drop(df[df["lieux"]==""].index, axis = 0, inplace=True)
-    df = df.reset_index(drop=True)
+    def level_theme(self):
+        self.df.loc[(self.df["theme"] =="randonnée")|(self.df["theme"] =="trek")|(self.df["theme"] =="auberge")|(self.df["theme"] =="hôtel")|(self.df["theme"] =="refuge")|(self.df["theme"] =="découverte"), "theme"] = "rando nature"
+        self.df.loc[(self.df["theme"] == "vélo")|(self.df["theme"] == "vtt")|(self.df["theme"] == "vélo de route"), "theme"] = "cyclo"
+        self.df.loc[(self.df["theme"] == "randonnée avec âne")|(self.df["theme"] == "chiens de traîneau")|(self.df["theme"] == "cani rando"), "theme"] = "ani-rando"
+        self.df.loc[(self.df["theme"] =="raquettes à neige")|(self.df["theme"] =="ski de randonnée")|(self.df["theme"] =="ski nordique")|(self.df["theme"] =="chalet")|(self.df["theme"] =="alpinisme")|(self.df["theme"] =="cascade de glace")|(self.df["theme"] =="raquette")|(self.df["theme"] =="logement privé"), "theme"] = "activités nordique"
+        for i in range(0, len(self.df)):
+            if "rando nature" not in self.df.iloc[i]["theme"] and "cyclo" not in self.df.iloc[i]["theme"] and "activités nordique" not in self.df.iloc[i]["theme"] and "multi-activités" not in self.df.iloc[i]["theme"] and "ani-rando" not in self.df.iloc[i]["theme"]: 
+                self.df.loc[i,"theme"] = "multi-activités"
 
-def transfo_var(df):
-    df["lieux"] = df["lieux"].astype("category")
-    df["diff"] = df["diff"].astype("category")
-    df["theme"] = df["theme"].astype("category")
+    def level_lieu(self):
+        for i in range(0, len(self.df)):
+            for str in region:
+                if str in self.df.loc[i,"lieux"]:
+                    self.df.loc[i, "lieux"] = "metropole"
+            for str in pays_proche:
+                if str in self.df.loc[i, "lieux"]:
+                    self.df.loc[i, "lieux"] = "dest_proche"
+        for i in range(0, len(self.df)):
+            if self.df.loc[i, "lieux"] != "metropole" and self.df.loc[i, "lieux"] != "dest_proche" and self.df.loc[i, "lieux"] != "" :
+                self.df.loc[i, "lieux"] = "dest_éloignée"
 
-def save_dataframe(df):
-    df.to_pickle("my_df.pkl")
+    def nettoyage(self):
+        self.df.drop(self.df[self.df["prix"]==0].index, axis = 0, inplace=True)
+        self.df.drop(self.df[self.df["lieux"]==""].index, axis = 0, inplace=True)
+        self.df = self.df.reset_index(drop=True)
 
+    def transfo_var(self):
+        self.df["lieux"] = self.df["lieux"].astype("category")
+        self.df["diff"] = self.df["diff"].astype("category")
+        self.df["theme"] = self.df["theme"].astype("category")
 
+    def save_dataframe(self):
+        self.df.to_pickle("my_df.pkl")
